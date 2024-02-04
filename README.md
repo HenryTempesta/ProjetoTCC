47a2d036fcc523ae68c7dfb7d41c88d88148f2ee
Henry Tempesta <henrytempestaa@gmail.com>
2024-02-04 16:49:28 -0300

    Primeira versão do sistema

---
+++ README.md
@@ -0,0 +1,8 @@
+Seja bem vindo ao meu sistema que produzi para meu TCC, a principio foi utlizado um bootstrap para ser utilizado como base, no final do projeto o bootstrap foi quase 100% modificado, utilimos HTML, CSS, javaScript e PHP para fazer o sistema
+
+ Implementamos diversas funções para o sistema, afim de deixar o mais personalizado para o nosso cliente para que ele posso desfrutar 100% do sistema e atender todas suas necessidades.
+
+ Implemtações que fizemos no site foi:
+ 1- API via cep para trazer as informações de forma automatica no cadastro
+ 2- Agenda para que o nosso cliente pudesse marcar suas tarefas e trabalhos
+  
---
+++ menu.php
@@ -0,0 +1,73 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Início</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+           
+            
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                    aria-expanded="true" aria-controls="collapsePages">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Cadastros</span>
+                </a>
+                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Login Screens:</h6>
+                        <a class="collapse-item" href="login.html">Login</a>
+                        <a class="collapse-item" href="register.html">Registrar</a>
+                        <a class="collapse-item" href="forgot-password.html">Esqueci a senha</a>
+                        <div class="collapse-divider"></div>
+                        <h6 class="collapse-header">Outras páginas:</h6>
+                        <a class="collapse-item" href="404.html">Error 404</a>
+                        <a class="collapse-item" href="blank.html">Página em branco</a>
+                        <a class="collapse-item" href="cadastroTeste.html">Cadastro Fornecedor</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Gráficos</span></a>
+            </li>
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item">
+                <a class="nav-link" href="tables.html">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Estoque</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+            <!-- Sidebar Message -->
+            
+
+        </ul>
